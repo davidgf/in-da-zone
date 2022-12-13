556a1d39cf53d7328a898be53c47170e8a13c786
David García <davidgf1987@gmail.com>
2022-12-13 16:28:33 +0100

    Improve dev script

--- packages/idz-popup/src/Timer.tsx
+++ packages/idz-popup/src/Timer.tsx
@@ -1,9 +1,22 @@
 import React, { useState, useEffect } from 'react'
 import * as browser from 'webextension-polyfill'
-import { TimerState } from 'idz-shared'
+import { TimerState, TimerStatus } from 'idz-shared'
+
+const defaultState: TimerState = {
+  duration: 0,
+  remaining: 0,
+  status: TimerStatus.Stopped
+}
 
 export default function Timer (): JSX.Element {
-  const [timerState, setTimerState] = useState<TimerState>()
+  const [timerState, setTimerState] = useState<TimerState>(defaultState)
+  const [remainingMinutes, setRemainingMinutes] = useState<number>(0)
+  const [remainingSeconds, setRemainingSeconds] = useState<number>(0)
+
+  useEffect(() => {
+    setRemainingMinutes(Math.floor(timerState.remaining / 60))
+    setRemainingSeconds(timerState.remaining % 60)
+  })
 
   function startBlocking (): void {
     void browser.runtime.sendMessage({ startBlocking: true })
@@ -28,6 +41,7 @@ export default function Timer (): JSX.Element {
   return (
     <div>
       <h2>Hi from the timer</h2>
+      <p>{remainingMinutes}:{remainingSeconds}</p>
       <p>{JSON.stringify(timerState)}</p>
       <button onClick={startBlocking}>Enter da zone</button>
     </div>
